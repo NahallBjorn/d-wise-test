88e0a7225f58a3fe1e642023ecc97db93981c778
NicolasGB <nicolas.rene@bairesdev.com>
2022-01-12 08:35:12 -0500

    adding init files

---
+++ src/App.svelte
@@ -0,0 +1,36 @@
+<script lang='ts'>
+    import OddSum from "./OddSum.svelte";
+
+    let result = 0;
+
+</script>
+
+<div class="container">
+    <div class="backbone">
+        <OddSum bind:sum={result} />
+        <p class="label"> Sum: {result} </p>
+    </div>
+</div>
+
+
+<style>
+    .container {
+        margin-top: 100px;
+        display: flex;
+        justify-content: center;
+    }
+    .backbone {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        padding-top: 50px;
+        width: 500px;
+        height: 400px;
+        background-color: #1FA2FF;
+    }
+    .label {
+        font-size: 21px;
+        color: #f4f4f4;
+        font-family: Monaco;
+    }
+</style>
---
+++ src/OddSum.svelte
@@ -0,0 +1,29 @@
+<script lang='ts'>
+    import Textfield from '@smui/textfield';
+
+    export let sum;
+
+    let maxInput = Number.MAX_VALUE;
+    let inputNumber = 0;
+    $: sum = calcOddSum(inputNumber);
+
+    function calcOddSum(topNumber: number): number {
+        let sum = 0;
+        if (topNumber <= 0) {
+            return 0;
+        }
+        if (topNumber % 2 == 0) {
+            topNumber = topNumber - 1;
+        }
+        for (let i = topNumber; i >= 1; i--) {
+            sum = sum + i;
+            i--;
+        }
+        return sum;
+    }
+
+</script>
+
+
+<Textfield bind:value={inputNumber} type="number"
+           input$min="0" input$max="{maxInput}"/>
